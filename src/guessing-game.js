515f80b4f4ad83bea104f9ad9401e8f8f70fa06f
Bulation <insanbulat@gmail.com>
2021-08-28 20:45:35 +0300

    feat: solve problem

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
     constructor() {}
-
+    number = null;
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = Math.ceil((this.min + this.max) / 2);
     }
 
     greater() {
-
+        this.min = Math.ceil((this.min + this.max) / 2);
     }
 }
 
